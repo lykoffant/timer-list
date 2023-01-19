5ce8f4f1d6aae35dbbf7c231049da77576a3e201
Anton Lykov <99159998+lykoffant@users.noreply.github.com>
2023-01-19 18:14:14 +0300

    feat: add a link to the project repository

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import GithubCorner from 'react-github-corner';
+
 import styles from './App.module.scss';
 import { AddTimerForm, TimerList } from './components';
 
@@ -9,6 +11,7 @@ function App() {
         <AddTimerForm className={styles.form} />
         <TimerList className={styles.list} />
       </div>
+      <GithubCorner href='https://github.com/lykoffant/timer-list' size={60} />
     </div>
   );
 }
